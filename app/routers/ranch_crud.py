491e04d13ca1c6baa3813c8c5363976cead38fe0
Dimitris <dimitristheodorou567@yahoo.com>
2024-05-03 16:03:13 +0300

    Put request method for ranch

--- app/routers/ranch_crud.py
+++ app/routers/ranch_crud.py
@@ -1,7 +1,7 @@
 from app.database import SessionLocal
 from app.models import Users, Ranches, UserRanches
 from .auth import get_current_user
-from fastapi import Depends, APIRouter, HTTPException
+from fastapi import Depends, APIRouter, HTTPException, Path
 from typing import Union, Annotated
 from sqlalchemy import text
 from starlette import status
@@ -15,8 +15,12 @@ router = APIRouter(
     tags=['Ranch']
 )
 
-class CreateRanchRequest(BaseModel):
+class RanchRequest(BaseModel):
     name: str
+    farm_id: str
+    main_animal_species: str
+    milked_animals: int
+    milk_yield_per_year: float
 
 
 #Function for opening and closing connection with the database after each query.
@@ -44,7 +48,7 @@ async def read_all_ranches_based_on_role(user:user_dependency, db: db_dependency
 
 
 @router.post('/')
-async def create_ranch(user: user_dependency, db: db_dependency, create_ranch_request: CreateRanchRequest):
+async def create_ranch(user: user_dependency, db: db_dependency, create_ranch_request: RanchRequest):
 
     if user is None:
         raise HTTPException(status_code=401, detail='Authentication Failed')
@@ -52,13 +56,30 @@ async def create_ranch(user: user_dependency, db: db_dependency, create_ranch_re
         name=create_ranch_request.name,
         created_at=datetime.now(),
         updated_at=datetime.now(),
+        farm_id=create_ranch_request.farm_id,
+        main_animal_species=create_ranch_request.main_animal_species,
+        milked_animals=create_ranch_request.milked_animals,
+        milk_yield_per_year=create_ranch_request.milk_yield_per_year
     )
     db.add(create_ranch_model)
     db.commit()
 @router.put("/{ranch_id}")
-async def update_ranch(user: user_dependency,db: db_dependency):
+async def update_ranch(user: user_dependency,db: db_dependency, update_ranch_request: RanchRequest, ranch_id: int = Path(gt=0)):
     if user is None:
         raise HTTPException(status_code=401, detail='Authentication Failed')
+    ranch_model = db.query(Ranches).filter(Ranches.id == ranch_id).first()
+    if ranch_model is None:
+        raise HTTPException(status_code=404, detail='Ranch not found.')
+
+    ranch_model.name = update_ranch_request.name
+    ranch_model.farm_id = update_ranch_request.farm_id
+    ranch_model.main_animal_species = update_ranch_request.main_animal_species
+    ranch_model.milked_animals = update_ranch_request.milked_animals
+    ranch_model.milk_yield_per_year = update_ranch_request.milk_yield_per_year
+    ranch_model.updated_at = datetime.now()
+
+    db.add(ranch_model)
+    db.commit()
 
 @router.delete("/{ranch_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_ranch(user:user_dependency,db: db_dependency, ranch_id: int):
