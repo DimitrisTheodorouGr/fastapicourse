abbb3d2d71d2cb31c1cf3e550e1f21f2486036ff
Dimitris <dimitristheodorou567@yahoo.com>
2024-05-03 18:51:07 +0300

    Get all association with farm name and rancher username

--- app/routers/admins.py
+++ app/routers/admins.py
@@ -1,7 +1,7 @@
 from starlette import status
 from .auth import get_current_user
 from app.database import SessionLocal
-from app.models import Users
+from app.models import Users, UserRanches, Ranches
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, Path
 from pydantic import BaseModel, Field
@@ -24,6 +24,37 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
+@router.get('/ranch_associations')
+async def get_user_ranch_associations(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    if user.get('user_role') == 'admin':
+        # Perform the query
+        results = (
+            db.query(
+                Users.username,
+                Users.role,
+                Ranches.name.label('ranch_name'),
+                Ranches.id.label('ranch_id'),
+                Users.id.label('user_id')
+            )
+            .join(UserRanches, UserRanches.user_id == Users.id)
+            .join(Ranches, UserRanches.ranch_id == Ranches.id)
+            .all()
+        )
+
+        # Transform the results into a list of dictionaries to return as JSON
+        output = [
+            {
+                "username": result.username,
+                "ranch_name": result.ranch_name,
+                "user_role":result.role,
+                "ranch_id": result.ranch_id,
+                "user_id": result.user_id
+            }
+            for result in results
+        ]
+        return output
 
 @router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(user: user_dependency, db: db_dependency, user_id: int = Path(gt=0)):
